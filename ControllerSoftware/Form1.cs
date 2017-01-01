486c2cd21b409e87336beb0eb21f5b4c5f6a9564
congxi <fsensor4git@sina.com>
2017-01-01 19:38:03 +0800

    create a project

--- ControllerSoftware/Form1.cs
+++ ControllerSoftware/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//using CCWin;
 namespace ControllerSoftware
 {
     public partial class Form1 : Form
